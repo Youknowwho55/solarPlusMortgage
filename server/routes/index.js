95cb13665601100ba54e9e06139ae959bbdb696f
Youknowwho55 <dylan9112@gmail.com>
2023-11-24 17:15:38 -0800

    mortgage and solar tabs

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,8 @@ const dashController = require("../controllers/dashController");
  * App Routes
  */
 router.get("/", dashController.dashSales);
+router.get("/solar", dashController.dashSolar);
+
 router.get("/admin", dashController.getAdmin);
 router.get("/processing", dashController.dashProcessing);
 router.get("/settings", dashController.dashSettings);
